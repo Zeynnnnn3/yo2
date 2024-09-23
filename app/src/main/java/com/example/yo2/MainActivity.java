52efc227ba5266bc074586e84fa11b8393602372
Zeynnnnn3 <zeyntagizade@gmail.com>
2024-09-22 22:07:47 -0700

    I added a button to change the colour of the text.

--- app/src/main/java/com/example/yo2/MainActivity.java
+++ app/src/main/java/com/example/yo2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.yo2;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvTextChange;
+    Button btnTextChange;
+    Button button2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        this.tvTextChange = findViewById(R.id.tvTextChange);
+        this.btnTextChange = findViewById(R.id.btnTextChange);
+        this.btnTextChange.setOnClickListener(view  -> tvTextChange.setText("After result"));
+        this.button2 = findViewById(R.id.button2);
+        this.button2.setOnClickListener(view -> tvTextChange.setTextColor(Color.parseColor("#FFC0CB")));
+
     }
+
+
 }
